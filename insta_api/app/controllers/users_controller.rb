23de41d6e2f64b43b1a0542cf9e271fa08961a31
Dan Eversen <daneversen@gmail.com>
2016-05-23 15:02:01 -0400

    working httparty request

--- insta_api/app/controllers/users_controller.rb
+++ insta_api/app/controllers/users_controller.rb
@@ -4,8 +4,6 @@ require 'HTTParty'
 
 class UsersController < ActionController::Base
 
-CALLBACK_URL = "http://localhost:3000/callback"
-
 
   def index
     @map_api_key = ENV['GOOGLE_MAP_API']
@@ -14,17 +12,18 @@ CALLBACK_URL = "http://localhost:3000/callback"
     @red = 'http://localhost:3000/callback'
   end
 
+
   def callback
     @id = ENV['IG_CLIENT_ID']
     @sec = ENV['IG_SECRET']
     @red = 'http://localhost:3000/callback'
     @act = '40264134.1677ed0.df3ba771313644258e0d661b5841395c'
-    response = HTTParty.get("https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=#{@act}")
+    @response = HTTParty.get("https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=#{@act}")
     binding.pry
+    render :callback
   end
 
   def show
-    #debugger
   end
 
 end
